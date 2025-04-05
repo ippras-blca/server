0a0d91e7a8386432d8de3f0fada23b703239b4ca
kgv <kgv@users.noreply.github.com>
2025-04-05 21:46:11 +0300

    Bump clap version to 4.5.35 and tokio version to 1.44.2

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -10,31 +10,34 @@ use std::sync::Arc;
 /// does not error on a missing `Content-Length` header.
 #[tokio::main]
 async fn main() -> Result<()> {
+    github().await
+}
+
+async fn github() -> Result<()> {
     // https://github.com/ippras-blcs/storage/blob/main/temperature/2025-03-28-13-22-02.log.parquet
     let http_store = HttpBuilder::new()
         .with_url("https://raw.githubusercontent.com/ippras-blcs/storage/refs/heads/main")
         .build()?;
+    let result = http_store
+        .get_opts(
+            &Path::parse("temperature/2025-03-28-13-22-02.log.parquet")?,
+            GetOptions {
+                // range: Some(GetRange::Bounded(0..100)),
+                ..Default::default()
+            },
+        )
+        .await?;
+    println!("result: {result:?}");
+    let bytes = result.bytes().await?;
+    println!("bytes: {bytes:?}");
 
-    // let result = http_store
-    //     .get_opts(
-    //         &Path::parse("temperature/2025-03-28-13-22-02.log.parquet")?,
-    //         GetOptions {
-    //             // range: Some(GetRange::Bounded(0..100)),
-    //             ..Default::default()
-    //         },
-    //     )
-    //     .await?;
-    // println!("result: {result:?}");
-    // let bytes = result.bytes().await?;
-    // println!("bytes: {bytes:?}");
-
-    let prefix = Path::from("temperature");
-    // Get an `async` stream of Metadata objects:
-    let mut list = http_store.list(Some(&prefix));
-    // Print a line about each object
-    while let Some(meta) = list.next().await.transpose()? {
-        println!("Name: {}, size: {}", meta.location, meta.size);
-    }
+    // let prefix = Path::from("temperature");
+    // // Get an `async` stream of Metadata objects:
+    // let mut list = http_store.list(Some(&prefix));
+    // // Print a line about each object
+    // while let Some(meta) = list.next().await.transpose()? {
+    //     println!("Name: {}, size: {}", meta.location, meta.size);
+    // }
 
     // let t = http_store
     //     .put(&Path::parse("TEST.parquet")?, PutPayload::from_bytes(bytes))
