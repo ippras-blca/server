c02f099b092906dba928167a845c9e7d06959f2f
kgv <kgv@users.noreply.github.com>
2025-04-06 22:16:13 +0300

    0

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,17 @@
 use anyhow::Result;
 use futures_util::stream::StreamExt;
 use object_store::{GetOptions, GetRange, ObjectStore, PutPayload, http::HttpBuilder, path::Path};
-use parquet::arrow::async_writer::ParquetObjectWriter;
-use std::sync::Arc;
+use parquet::{
+    arrow::{
+        ParquetRecordBatchStreamBuilder,
+        async_reader::{AsyncFileReader, ParquetObjectReader},
+        async_writer::ParquetObjectWriter,
+    },
+    file::reader::SerializedFileReader,
+};
+use polars::{io::SerReader as _, prelude::ParquetReader};
+use std::{io::Cursor, sync::Arc};
+use tokio_util::bytes::Buf as _;
 
 // ba9ef89ad1700e99743edbe3cc896b17
 // gxvfqijtupobvequ
@@ -14,22 +23,31 @@ async fn main() -> Result<()> {
 }
 
 async fn github() -> Result<()> {
-    // https://github.com/ippras-blcs/storage/blob/main/temperature/2025-03-28-13-22-02.log.parquet
+    // https://github.com/ippras-blcs/storage/blob/main/temperature/2025-03-28-17-11-35.log.parquet
     let http_store = HttpBuilder::new()
         .with_url("https://raw.githubusercontent.com/ippras-blcs/storage/refs/heads/main")
         .build()?;
+    let path = Path::parse("temperature/2025-03-28-17-11-35.log.parquet")?;
     let result = http_store
         .get_opts(
-            &Path::parse("temperature/2025-03-28-13-22-02.log.parquet")?,
+            &path,
             GetOptions {
                 // range: Some(GetRange::Bounded(0..100)),
                 ..Default::default()
             },
         )
         .await?;
-    println!("result: {result:?}");
     let bytes = result.bytes().await?;
-    println!("bytes: {bytes:?}");
+    let mut reader = ParquetReader::new(Cursor::new(bytes));
+    let meta = reader.get_metadata()?;
+    println!("meta: {meta:?}");
+    let data = reader.finish()?;
+    println!("data: {data}");
+
+    // let meta = http_store.head(&path).await?;
+    // let reader = ParquetObjectReader::new(Arc::new(http_store), meta);
+    // let stream = ParquetRecordBatchStreamBuilder::new(reader).await?.build()?;
+    // let t = stream.next_row_group().await?;
 
     // let prefix = Path::from("temperature");
     // // Get an `async` stream of Metadata objects:
